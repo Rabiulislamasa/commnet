cbf3e61a86747a066c4c1463e76b3124a991f1f7
MD Rabiul Islam (Asa) <60613013+Rabiulislamasa@users.noreply.github.com>
2021-07-09 01:26:08 +0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,37 +1,16 @@
-                                <div class="comments-area">
-                                    <div class="comments-section">
-                                        <h3 class="comments-title">Posts Comments</h3>
-                                        <ol class="comments">
-                                        <?php
-                wp_list_comments( array(
-                    'style'       => 'li',
-                    'short_ping'  => true,
-                    'avatar_size' => 74,
-                ) );
-            ?>
-                                        </ol>
-                                    </div> <!-- end comments-section -->
-                                </div>
-
-                                <div class="comment-respond">
+                                    <div class="comment-respond">
                                         <h3 class="comment-reply-title">Write a Comment</h3>
                                         <form method="post" id="commentform" class="comment-form">
-                                            <?php $comments_args = array(
-                                        
-                                                'comment_field' => '<div class="form-textarea">
+                                            <div class="form-textarea">
                                                 <textarea id="comment" placeholder="Write Your Comments..."></textarea>
-                                                </div>',
-                                                'fields' => apply_filters( 'comment_form_default_fields', array(
-                                                
-                                                        'url'  => '<div class="form-inputs">
-                                                        <input placeholder="Website" type="url">',
-                                                        'name'  => '<input placeholder="Name" type="text">',
-                                                            
-                                                        'email'    => '<input placeholder="Email" type="email">
-                                                        </div>',
-                                                        )
-                                                    ),
-                                            );
-                                            comment_form($comments_args);  ?>
+                                            </div>
+                                            <div class="form-inputs">
+                                                <input placeholder="Website" type="url">
+                                                <input placeholder="Name" type="text">
+                                                <input placeholder="Email" type="email">
+                                            </div>
+                                            <div class="form-submit">
+                                                <input id="submit" value="Post Comment" type="submit">
+                                            </div>
                                         </form>
                                     </div>
